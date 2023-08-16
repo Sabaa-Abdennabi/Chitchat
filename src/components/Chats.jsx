03385a6e7cfe1001eb5eaf3fc9eee273f2a0bf1a
Sabaa-Abdennabi <saba.abdennebi@insat.ucar.tn>
2023-08-16 09:27:35 +0200

    "added chats in the sidebar"

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Chats = () => {
   return (
-    <div>
-      
+    <div className='chats'>
+      <div className="userChat">
+        <img src="https://i.pinimg.com/474x/1f/83/c5/1f83c5ce9090c12d1969ad7a3745cc82.jpg" alt="" />
+        <div className="userChatinfo">
+          <span>Sabaa</span>
+          <p>Hello</p>
+        </div>
+      </div>
     </div>
   )
 }
