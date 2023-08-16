d5a7df0268f35711cab952f63d01b375356eab6c
Sabaa-Abdennabi <saba.abdennebi@insat.ucar.tn>
2023-08-16 09:56:11 +0200

    "added top of chat"

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Cam from '../images/cam.png'
+import Add from '../images/add.png'
+import More from '../images/more.png'
 const Chat = () => {
   return (
     <div className='chat'>
       <div className="chatinfo">
         <span>Sabaa</span>
         <div className="chatIcons">
-
-
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
         </div>
       </div>
     </div>
