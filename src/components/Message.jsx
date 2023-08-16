f197a851af28c9e8e7f8868c3c28c5e7a9dfc718
Sabaa-Abdennabi <saba.abdennebi@insat.ucar.tn>
2023-08-16 11:04:55 +0200

    "added message from the owner and sender"

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>
-      Message
+ 
+    <div className='message'>
+      <img src="https://i.pinimg.com/474x/1f/83/c5/1f83c5ce9090c12d1969ad7a3745cc82.jpg" alt="" />
+      <div className="messageinfo">
+        <p>Hi, I'm a message</p>
+      </div>
     </div>
+
   )
 }
 
