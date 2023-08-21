de35faa2206a32b77c493e0bd3026165d2a38053
Sabaa-Abdennabi <saba.abdennebi@insat.ucar.tn>
2023-08-21 18:35:04 +0200

    trying to fix a chat db error

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,62 +1,74 @@
-import {useState} from 'react'
-import { collection,query,where,getDocs } from 'firebase/firestore';
-import {db} from '../firebase';
+import { useState } from 'react'
+import { collection, query, where, setDoc,getDoc, getDocs,updateDoc, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase';
 import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
+import { doc } from 'firebase/firestore';
 const Search = () => {
   const [username, setUsername] = useState('');
-  const[user, setUser]=useState(null);
+  const [user, setUser] = useState(null);
   const [err, setErr] = useState(false);
-  const {currentUser}=useContext(AuthContext);
-const handleKey=(e)=>{
- e.code=="Enter"&& handleSearch();
+  const { currentUser } = useContext(AuthContext);
+  const handleKey = (e) => {
+    e.code == "Enter" && handleSearch();
 
-}
-const handleSelect=async ()=>{
-  //check if there is a chat
-  const combinetid=currentUser.uid> user.uid? currentUser.uid+user.uid:user.uid+currentUser.uid;
-  try {
-  const res= await getDocs(db,"chats",combinetid);
-if(!res.exists()){
-  //create chat
-  await setDoc(doc,(db,"chats",combinetid),{messages:[]});
-  //create userChat
-  userChats: {
-    JanesId:{
-      combinetid:{
-        userInfo:{
-          DelayNode
-          ,img
-          ,id
-        },
-        lastMessage:"",
-        date:""
+  }
+  const handleSelect = async () => {
+    //check if there is a chat
+    const combinetid = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db, "chats", combinetid));
+      
+      if (!res.exists()) {
+
+        //create chat
+
+        await setDoc(doc(db, "chats", combinetid), { messages: [] });
+
+        //create userChat
+
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinetid + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinetid + ".date"]: serverTimestamp(),
+        })
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinetid + ".userinfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinetid + ".date"]: serverTimestamp(),
+        })
+
+
+
+
       }
+    } catch (err) {
+      setErr(true);
+      console.log(err);
     }
+    // create userchat
+
   }
+  const handleSearch = async () => {
+    const q = query(collection(db, "users"), where("displayName", "==", username));
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+        console.log(doc.id, " => ", doc.data());
+      });
+    } catch (err) {
+      setErr(true);
+      console.log(err);
+    }
 
-
-
-}
-  }catch(err){
-    setErr(true);
-    console.log(err);
   }
-  // create userchat
-
-}
-const handleSearch=async ()=> {
-  const q = query(collection(db, "users"), where("displayName", "==", username));
-  try{
-  const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
-    setUser(doc.data());
-    console.log(doc.id, " => ", doc.data());
-  });}catch(err){
-    setErr(true); 
-    console.log(err);
-  }
-
-}
 
 
 
@@ -65,16 +77,16 @@ const handleSearch=async ()=> {
   return (
     <div className='search'>
       <div className="searchForm">
-              <input type='text' placeholder='Search' onKeyDown={handleKey}onChange={e=>setUsername(e.target.value)} />
+        <input type='text' placeholder='Search' onKeyDown={handleKey} onChange={e => setUsername(e.target.value)} />
       </div>
-      {err&&<span className='err'>User not found</span>}
-      {user&&<div className="userChat" onClick={handleSelect}>
+      {err && <span className='err'>User not found</span>}
+      {user && <div className="userChat" onClick={handleSelect}>
         <img src={user.photoURL} alt="" />
         <div className="userChatinfo">
           <span>{user.displayName}</span>
         </div>
       </div>}
-      
+
     </div>
   )
 }
